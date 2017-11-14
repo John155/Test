b41daa628f7db39a3fc0a66921919e6042ad908b
John155 <john.ivan.fox@outlook.com>
2017-11-14 16:04:55 +0100

    Tage hinzugefügt

--- public/javascripts/angularUIController.js
+++ public/javascripts/angularUIController.js
@@ -1,12 +1,19 @@
 
 var my = angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
 
-    my.controller('AppCtrl', function($scope) {
+my.controller('AppCtrl', function($scope) {
 
-    });
+});
 
 
-    my.controller('cltmy', function() {
+my.controller('cltmy', function() {
     this.myDate = new Date();
     this.isOpen = false;
+
 });
+
+my.controller('DaysTest' ,function ($scope) {
+    $scope.names = [{Name: 'Monatg'}, {Name: 'Dinestag'}, {Name: 'Mittwoch'}, {Name: 'Donnerstag'}, {Name: 'Freitag'}, {Name: 'Samstag'}, {Name: 'Sonnstag'}];
+});
+
+
